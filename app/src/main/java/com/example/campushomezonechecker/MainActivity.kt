a434f9800342738ed3a840a08662292ff3f06178
Maleesha Rajasooriya <rajasoo-IM23058@stu.kln.ac.lk>
2026-08-19 09:28:00 +0530

    Polish zone checker responsive UI

--- app/src/main/java/com/example/campushomezonechecker/MainActivity.kt
+++ app/src/main/java/com/example/campushomezonechecker/MainActivity.kt
@@ -4,10 +4,11 @@ import android.app.Activity
 import android.os.Bundle
 
 class MainActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Member 1 only loads the UI. Button behavior will be added later.
+        // Button and location behavior will be implemented by later members.
     }
 }
