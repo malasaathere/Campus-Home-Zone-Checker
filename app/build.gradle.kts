0fb78f49eb4ce8d82ec5661758c79ce1bcb5f12b
Maleesha Rajasooriya <rajasoo-IM23058@stu.kln.ac.lk>
2026-08-19 08:56:38 +0530

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.group2.campuszonechecker"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "com.group2.campuszonechecker"
+        minSdk = 24
+        targetSdk = 37
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    // Added now so later members can use FusedLocationProviderClient.
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+}
---
+++ app/src/main/java/com/example/campushomezonechecker/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.group2.campuszonechecker
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Member 1 only loads the UI. Button behavior will be added later.
+    }
+}
